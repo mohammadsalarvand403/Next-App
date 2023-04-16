149ef1bbeb32c942627c0786d9d0a4b98d2c7f5d
mohammadsalarvand403 <mmdslv7707@gmail.com>
2023-04-17 01:20:42 +0430

    styling to category

--- src/components/post/CaregoryDesktop.jsx
+++ src/components/post/CaregoryDesktop.jsx
@@ -1,8 +1,10 @@
 import {ChevronDownIcon} from "@heroicons/react/24/outline"
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 const CategoryDeskTop = ({postCategoris}) => {
     const [isOpen,setIsOpen]=useState(true)
+    const {query}=useRouter()
     return ( 
         <div className="bg-white rounded-3xl overflow-hidden">
         <div className="flex items-center justify-between py-4 px-4 bg-purple-400"
@@ -13,12 +15,15 @@ const CategoryDeskTop = ({postCategoris}) => {
         />
         </div>
         <div className={`${isOpen ? "block":"hidden"}`}>
-          <Link href={"/blogs"} className="block px-2 hover:bg-purple-100 py-2 mb-1" >
+          <Link href={"/blogs"} className={` block px-2 hover:bg-purple-100 py-2 mb-1
+          ${!query.categorySlug ?
+            "bg-purple-700 text-white hover:bg-purple-500":""}`} >
            همه مقالات
           </Link>
           {postCategoris.map((category=>{
             return(
-              <Link key={category.id} href={`/blogs/${category.englishTitle}`} className="block px-2 hover:bg-purple-100 py-2 mb-1" >
+              <Link key={category.id} href={`/blogs/${category.englishTitle}`} className={`block px-2 hover:bg-purple-100 py-2 mb-1 ${query.categorySlug ===category.englishTitle ?
+              "bg-purple-700 text-white hover:bg-purple-500":""}`}>
            {category.title}
           </Link>
             )
--- src/components/post/CategoryMobile.jsx
+++ src/components/post/CategoryMobile.jsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CategoryMobile = ({postCategoris}) => {
+  const {query}=useRouter()
     return ( 
     <div className=" flex md:hidden gap-x-4 overflow-auto pt-5 pb-5">
+        <Link href={"/blogs"} className={`block text-sm whitespace-nowrap px-3 py-1 border border-gray-500 text-center  bg-white rounded-3xl text-gray-500 whitespace-nowrap text-sm
+          ${!query.categorySlug ?
+            "border-purple-700 bg-purple-700  text-white hover:bg-purple-500 border-2":""}`} >
+           همه مقالات
+          </Link>
       {postCategoris.map((category=>{
         return(
           <Link key={category.id} href={`/blogs/${category.englishTitle}`}>
-      <p className="block text-sm whitespace-nowrap px-3 py-1 border border-gray-500 text-center bg-white rounded-3xl text-gray-500" > {category.title}</p>
+      <p className={`block text-sm whitespace-nowrap px-3 py-1 border border-gray-500 text-center  bg-white rounded-3xl text-gray-500 whitespace-nowrap text-sm" ${query.categorySlug ===category.englishTitle ?
+      "border-purple-700 bg-purple-700 border-2 text-white hover:bg-purple-500":""} `}> {category.title}</p>
       </Link>
         )
       }))}
